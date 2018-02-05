033bc272d18bf6885d9e843a42bda36d56ca50b4
William Graham <w.m.graham24@gmail.com>
2018-02-05 12:12:13 +0000

    added event routes to backend, tested backend routes with insomnia, added secureRoute, removed required fields from event model

--- models/event.js
+++ models/event.js
@@ -4,14 +4,14 @@ const itemSchema = new mongoose.Schema({
   itemName: { type: String, required: true },
   amount: { type: String },
   bringer: { type: mongoose.Schema.ObjectId, ref: 'User' },
-  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User' }
 });
 
 itemSchema.set('toJSON', { virtuals: true });
 
 const commentSchema = new mongoose.Schema({
   content: { type: String, required: true },
-  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User' }
 }, {
   timestamps: true
 });
@@ -21,13 +21,13 @@ commentSchema.set('toJSON', { virtuals: true });
 const eventSchema = new mongoose.Schema({
   eventName: { type: String, required: true },
   decsription: { type: String },
-  date: { type: Date, required: true },
+  date: { type: Date },
   location: { type: String, required: true },
   eventKey: {},
   guests: [],
   items: [ itemSchema ],
   comments: [ commentSchema ],
-  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User' },
   photos: []
 });
 
