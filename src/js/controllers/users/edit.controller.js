92dd83a647d6760651c490a609ae68505fc3aebc
Omar Portillo <OmrPrt@aol.com>
2018-02-05 14:53:46 +0000

    user profile updating and diplaying

--- src/js/controllers/users/edit.controller.js
+++ src/js/controllers/users/edit.controller.js
@@ -2,9 +2,25 @@ angular
   .module('bringItApp')
   .controller('EditUserCtrl', EditUserCtrl);
 
-
 EditUserCtrl.$inject = ['User', '$state'];
 function EditUserCtrl(User, $state) {
   const vm = this;
   vm.user = User.get($state.params);
+
+  User.get($state.params)
+    .$promise
+    .then((user) => {
+      vm.user = user;
+    });
+
+  vm.update = userUpdate;
+
+  function userUpdate() {
+    User
+      .update($state.params, vm.user)
+      .$promise
+      .then(() => {
+        $state.go('userShow', $state.params);
+      });
+  }
 }
