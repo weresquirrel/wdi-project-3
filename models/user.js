edb881af12b6ac078fa442ef048a192548e029f0
Omar Portillo <OmrPrt@aol.com>
2018-02-05 12:11:27 +0000

    made a basic user-profile show page

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  bio: { type: String },
   profilePic: {type: String }
 });
 
